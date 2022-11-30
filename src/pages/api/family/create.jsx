f9363b8de7045b59dc0f5e09190805639a1c623c
Rustom Ichhaporia <45826892+rustom@users.noreply.github.com>
2022-11-30 16:08:53 -0600

    Add explanation for new trigger

--- src/pages/api/family/create.jsx
+++ src/pages/api/family/create.jsx
@@ -5,20 +5,30 @@ import endpointWrapper from '../util'
 
 export default async function handler(req, res) {
   const inputData = JSON.parse(req.body)
-  console.log(inputData)
 
   const query = `
   insert into Family (leaderID, accessType, serviceName) 
   values (${inputData.leaderID}, '${inputData.accessType}', '${inputData.serviceName}')
   `.replace(/(\r\n|\n|\r)/gm, "")
 
-  // Buggy when user creates multiple of the same type of family
-  const membershipQuery = `insert into Membership (memberID, familyID, memberStatus) 
-  values (${inputData.leaderID}, (select familyID from Family where leaderID=${inputData.leaderID} and serviceName='${inputData.serviceName}' limit 1), 'Accepted')
-  `.replace(/(\r\n|\n|\r)/gm, "")
+  // The below membership creation query has been replaced by our SQL trigger, which was created in the GCP cloud console
+  // delimiter $$ 
+  // create trigger createFamily
+  // after insert on Family
+  // for each row
+  // begin
+  //     if exists (select * from Family f where f.leaderID=new.leaderID and f.serviceName=new.serviceName) then
+  //     insert into Membership (memberID, familyID, memberStatus) 
+  //     values (new.leaderID, (select familyID from Family q where q.leaderID=new.leaderID and q.serviceName=new.serviceName limit 1), 'Accepted');
+  //     end if;
+  // end $$
+  // delimiter;
 
-  console.log(query)
-  console.log(membershipQuery)
+
+  // // Buggy when user creates multiple of the same type of family
+  // const membershipQuery = `insert into Membership (memberID, familyID, memberStatus) 
+  // values (${inputData.leaderID}, (select familyID from Family where leaderID=${inputData.leaderID} and serviceName='${inputData.serviceName}' limit 1), 'Accepted')
+  // `.replace(/(\r\n|\n|\r)/gm, "")
 
   const session = await unstable_getServerSession(req, res, authOptions)
 
@@ -34,16 +44,9 @@ export default async function handler(req, res) {
     await connection.execute(query)
     await connection.execute(membershipQuery)
 
-    // + (req.query.id && (' where userId=' + req.query.id))
-    // )
-    // console.log(JSON.parse(JSON.stringify(rows)))
-
     await connection.end()
     // return res.send(JSON.parse(JSON.stringify(rows)))
   }
 
-
-  // await endpointWrapper(req, res, query)
-  // await endpointWrapper(req, res, membershipQuery)
   return {}
 }
